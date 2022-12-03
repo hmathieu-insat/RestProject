d8d779e2d436cc02e43a96381afbce050a6aa821
Hugo MATHIEU <hugo.mathieu@exakis-nelite.com>
2022-12-03 14:44:48 +0100

    Add more actions to the StudentsResource

--- src/main/java/com/INSAT/RestProject/StudentResource.java
+++ src/main/java/com/INSAT/RestProject/StudentResource.java
@@ -1,7 +1,9 @@
 package com.INSAT.RestProject;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
@@ -13,6 +15,24 @@ public class StudentResource {
         Student student = new Student();
         student.setName("Mohamed");
         student.setSurname("Bouhlel");
+        return student;
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Student getStudent(@PathParam("id") int id) {
+        Student student = new Student();
+        student.setName("Mohamed");
+        student.setSurname("Bouhlel");
+        return student;
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Student createStudent(Student student) {
+        System.out.println("Student created");
+        System.out.println(student);
         return student;
     }
 }
