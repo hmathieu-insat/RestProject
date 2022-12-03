c948abdfddba9d98907ce0c6f81f906530986916
Hugo MATHIEU <hugo.mathieu@exakis-nelite.com>
2022-12-03 15:07:24 +0100

    Adapt client to consume the controller

---
+++ RestProject.Client/Tasks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestProject.Client
+{
+    public class Tasks
+    {
+        public required string Title { get; set; }
+
+        public required Person Handler { get; set; }
+    }
+
+    public class Person
+    {
+        public required string Name { get; set; }
+
+        public required string Surname { get; set; }
+
+        public required string Function { get; set; }
+    }
+}
