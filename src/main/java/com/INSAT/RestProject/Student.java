182377ac41cc058b6bd2f9e82fef563a60ec4aee
Hugo MATHIEU <hugo.mathieu@exakis-nelite.com>
2022-12-03 14:34:59 +0100

    Add Class student

---
+++ src/main/java/com/INSAT/RestProject/Student.java
@@ -0,0 +1,25 @@
+package com.INSAT.RestProject;
+
+public class Student {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    private String surname;
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    private Pair pair;
+    public Pair getPair() {
+        return pair;
+    }
+    public void setPair(Pair pair) {
+        this.pair = pair;
+    }
+}
