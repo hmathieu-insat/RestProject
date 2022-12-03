847895e4c7d050ac8e28fe5e8775703296f7df29
Hugo MATHIEU <hugo.mathieu@exakis-nelite.com>
2022-12-03 14:28:40 +0100

    Add project files.

---
+++ RestProject.Client/HttpRequester.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace RestProject.Client
+{
+    class HttpRequester
+    {
+        private readonly HttpClient _client;
+        public HttpRequester(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<string> GetAsync(string endpoint)
+        {
+            var response = await _client.GetAsync(endpoint);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> PostAsync(string endpoint, string content)
+        {
+            var response = await _client.PostAsync(endpoint, new StringContent(content));
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> PutAsync(string endpoint, string content)
+        {
+            var response = await _client.PutAsync(endpoint, new StringContent(content));
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
